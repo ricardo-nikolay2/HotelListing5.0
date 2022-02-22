be48252f5d5f334b010f58b0cddfdf7376d12648
Ricardo Nikolay <Ricardo.Nikolay@gmail.com>
2022-02-22 13:30:22 +0700

    Insert data pada table Countries dan Hotels dengan menggunakan Add-Migration SeedingData

--- HotelListing5.0/Data/DatabaseContext.cs
+++ HotelListing5.0/Data/DatabaseContext.cs
@@ -12,60 +12,60 @@ namespace HotelListing5._0.Controllers.Data
         {
                         
         }
-        //protected override void OnModelCreating(ModelBuilder builder)
-        //{
-        //    builder.Entity<Country>().HasData(
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Country>().HasData(
 
-        //        new Country 
-        //        {
-        //            Id = 1, 
-        //            Name = "Jamaica",
-        //            Shortname = "JM"
-        //        },
-        //        new Country
-        //        {
-        //            Id = 2,
-        //            Name = "Bahamas",
-        //            Shortname = "BS"
-        //        },
-        //        new Country
-        //        {
-        //            Id = 3,
-        //            Name = "Cayman Island",
-        //            Shortname = "CI"
-        //        }
+                new Country
+                {
+                    Id = 1,
+                    Name = "Jamaica",
+                    Shortname = "JM"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "Bahamas",
+                    Shortname = "BS"
+                },
+                new Country
+                {
+                    Id = 3,
+                    Name = "Cayman Island",
+                    Shortname = "CI"
+                }
 
-        //        );
-        //    builder.Entity<Hotel>().HasData(
-                
-        //        new Hotel
-        //        {
-        //            Id = 1,
-        //            Name = "Sandals Resort and Spa",
-        //            Address = "Negril",
-        //            CountrId = 1,
-        //            Ranting = 4.5
-        //        },
-        //        new Hotel
-        //        {
-        //            Id = 2,
-        //            Name = "Comfort Suites",
-        //            Address = "George Town",
-        //            CountrId = 3,
-        //            Ranting = 4
-        //        },
+                );
+            builder.Entity<Hotel>().HasData(
 
-        //         new Hotel
-        //         {
-        //             Id = 3,
-        //             Name = "Grand Palldium",
-        //             Address = "Nassua",
-        //             CountrId = 2,
-        //             Ranting = 4
-        //         }
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Sandals Resort and Spa",
+                    Address = "Negril",
+                    CountrId = 1,
+                    Ranting = 4.5
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Comfort Suites",
+                    Address = "George Town",
+                    CountrId = 3,
+                    Ranting = 4
+                },
 
-        //        );
-        //}
+                 new Hotel
+                 {
+                     Id = 3,
+                     Name = "Grand Palldium",
+                     Address = "Nassua",
+                     CountrId = 2,
+                     Ranting = 4
+                 }
+
+                );
+        }
 
         public DbSet<Country> Countries { get; set; }
 
--- HotelListing5.0/Migrations/20220222061246_SeedingData.Designer.cs
+++ HotelListing5.0/Migrations/20220222061246_SeedingData.Designer.cs
@@ -3,14 +3,16 @@ using HotelListing5._0.Controllers.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace HotelListing5._0.Migrations
 {
     [DbContext(typeof(DatabaseContext))]
-    partial class DatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20220222061246_SeedingData")]
+    partial class SeedingData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
