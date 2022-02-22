fdddd78cf0f497e99036498485d191f37523522a
Ricardo Nikolay <Ricardo.Nikolay@gmail.com>
2022-02-22 15:39:46 +0700

    menambahkan file IRepository dan Repository

---
+++ HotelListing5.0/Repository/GenericRepository.cs
@@ -0,0 +1,69 @@
+﻿using HotelListing5._0.Controllers.Data;
+using HotelListing5._0.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace HotelListing5._0.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly DatabaseContext _context;
+
+        private readonly DbSet<T> _db;
+
+        public GenericRepository(DatabaseContext context)
+        {
+            _context = context;
+            _db = _context.Set<T>();    
+        }
+
+
+        public async Task Delete(int id)
+        {
+            var entity = await _db.FindAsync(id);
+            _db.Remove(entity);
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            _db.RemoveRange(entities);
+        }
+
+        public async Task<T> Get(Expression<Func<T, bool>> expression, List<string> includes = null)
+        {
+            IQueryable<T> query = _db;
+            if (includes != null)
+            {
+                foreach (var includePropery in includes)
+                {
+                    query = query.Include(includePropery);
+                }
+            }
+        }
+
+        public async Task<IList<T>> GetAll(Expression<Func<T, bool>> expression = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<string> includes = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task Insert(T entity)
+        {
+            await _db.AddAsync(entity);
+        }
+
+        public async Task InsertRange(IEnumerable<T> entities)
+        {
+           await _db.AddRangeAsync(entities);
+        }
+
+        public void update(T entity)
+        {
+            _db.Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+    }
+}
